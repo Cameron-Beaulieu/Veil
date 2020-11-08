07118038f35577b1645c5eece81de0cc56b578b8
EvCoding <ew18kilburn@gmail.com>
2020-11-07 19:40:31 -0500

    updated frontend image capture

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import { Button } from 'semantic-ui-react'
+import { Component } from 'react';
+import Webcam from 'react-webcam';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class ImageCapture extends Component{
+
+  constructor(props){
+      super(props);
+      this.state = { screenshot: null }
+  }
+  
+  myApiCall(){
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+      .then(response => response.json())
+      .then(json => console.log(json))
+  }
+
+  screenshot() {
+      // access the webcam trough this.refs
+      var screenshot = this.refs.webcam.getScreenshot();
+      this.setState({screenshot: screenshot});
+    }
+
+  render(){
+
+      return (
+          <div>   
+           <Webcam audio ={false} ref='webcam'/>
+           <button onClick={this.screenshot.bind(this)}>Capture Image</button>
+           <button onClick={this.myApiCall}>Mask Detection</button>
+           { this.state.screenshot ? <img src={this.state.screenshot} /> : null }
+          </div>
+          )
+  }
 }
-
-export default App;
