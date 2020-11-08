b7643a6216c92d5858ecffa081cc60ac9edc58a6
Brandon Ye <57378354+yebrandon@users.noreply.github.com>
2020-11-08 03:18:49 -0500

    testing out displaying to frontend

--- client/src/App.js
+++ client/src/App.js
@@ -17,34 +17,54 @@ export default class ImageCapture extends Component {
       .then((json) => console.log(json));
   }
 
-  saveImg(image){
-     // Simple POST request with a JSON body using fetch
-     const requestOptions = {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ "base64str": image})
-     };
-     console.log(requestOptions)
-  fetch('http://localhost:8000/saveImg', requestOptions)
-      .then(response => response.json())
+  saveImg(image) {
+    // Simple POST request with a JSON body using fetch
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ base64str: image }),
+    };
+    console.log(requestOptions);
+    fetch("http://localhost:8000/saveImg", requestOptions)
+      .then((response) => response.json())
       .then((json) => console.log(json));
   }
 
   screenshot() {
     // access the webcam trough this.refs
     var screenshot = this.refs.webcam.getScreenshot();
-    console.log(screenshot)
-    this.setState({ screenshot: screenshot })
-    this.saveImg(screenshot)
+    console.log(screenshot);
+    this.setState({ screenshot: screenshot });
+    this.saveImg(screenshot);
+  }
+
+  showFaces() {
+    for (entry in this.state.labels) {
+      labelStr = "";
+
+      if (entry[1] == "FaceTrainingSet") {
+        labelStr = "No mask";
+      } else {
+        labelStr = "Mask";
+      }
+      return (
+        <div>
+          <img src={"/tests/" + entry[0]} />
+          {labelStr}
+          {"confidence: " + entry[2]}
+        </div>
+      );
+    }
   }
 
   render() {
     return (
       <div>
-        <Webcam screenshotFormat={'image/png'} audio={false} ref="webcam" />
+        <Webcam screenshotFormat={"image/png"} audio={false} ref="webcam" />
         <button onClick={this.screenshot.bind(this)}>Capture Image</button>
         <button onClick={this.saveImg}>Mask Detection</button>
         {this.state.screenshot ? <img src={this.state.screenshot} /> : null}
+        {this.state.labels ? this.showFaces() : null}
       </div>
     );
   }
